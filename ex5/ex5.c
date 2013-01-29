2a370351e7b6fdc9f1028b9f3c500f405b558e3d
Keith Mascarenhas <mascarenhas.k@husky.neu.edu>
2013-01-29 09:57:17 -0500

    completed ex5

---
+++ ex5/ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Single line comment
+
+/*
+Multi
+Line
+Comment
+*/
+
+int main(int argc, char *args[])
+/* For the function to be totally complete it needs to return an int and take two 
+parameters, an int for the argument count and an array of char * strings for the arguments */
+{
+	int distance = 100;
+
+	printf("You are %d miles away.\n", distance);
+
+	return 0;
+}
