01d739f27a299144f71c4a88e1d3c16e93e6d4be
Keith Mascarenhas <mascarenhas.k@husky.neu.edu>
2013-01-29 10:16:20 -0500

    modified ex6.c to make note about trailing f in float declarations

--- ex6/ex6.c
+++ ex6/ex6.c
@@ -3,7 +3,9 @@
 int main(int argc, char *args[])
 {
 	int distance = 100;
-	float power = 2.345f;
+	float power = 2.345f; //the trailing f is indicates the value is a float
+			// if the f is ignored, power will be promoted to a double.
+			// http://stackoverflow.com/questions/5026592/1415352
 	double super_power = 56789.4532; //float and double both take %f format-specifier
 	char initial = 'A'; //char takes single-quotes
 	char first_name[] = "Zed"; //char[] needs double-quotes
